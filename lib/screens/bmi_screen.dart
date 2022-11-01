33e183e44058f33da636f51b7e9e851205276bfb
osama morad <osamamorad85@gmail.com>
2022-11-01 12:53:18 +0200

    BMI App

---
+++ lib/screens/bmi_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BmiScreen extends StatefulWidget {
+  const BmiScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BmiScreen> createState() => _BmiScreenState();
+}
+
+class _BmiScreenState extends State<BmiScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BMI Calculator',
+        ),
+      ),
+    );
+  }
+}
