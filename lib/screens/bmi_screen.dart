fde95ab3435a3ff05616b55a73240a14e590e9b4
osama morad <osamamorad85@gmail.com>
2022-11-01 13:45:08 +0200

    design male and female

--- lib/screens/bmi_screen.dart
+++ lib/screens/bmi_screen.dart
@@ -16,6 +16,66 @@ class _BmiScreenState extends State<BmiScreen> {
           'BMI Calculator',
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.grey[350],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/male.png'),
+                      width: 120.0,
+                      height: 120.0,
+                      //color: Colors.blue,
+                    ),
+                    Text(
+                      'Male',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 20.0,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.grey[350],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/female.png',
+                      width: 120.0,
+                      height: 120.0,
+                    ),
+                    Text(
+                      'Female',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
